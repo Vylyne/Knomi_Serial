64e2adb1dfb59cdf57994c9acd898ac137052457
Vylyne <94922829+Vylyne@users.noreply.github.com>
2026-06-30 16:36:09 -0400

    Move knomi_serial.py to klippy_extras and update install script for new path

--- install.sh
+++ install.sh
@@ -2,9 +2,11 @@
 
 EXTRA_PATH="$HOME/klipper/klippy/extras/knomi_serial.py"
 if [ -e "$EXTRA_PATH" ]; then
+  echo "removing existing $EXTRA_PATH."
   rm "$EXTRA_PATH"
 fi
-ln -s "$(pwd)/$(dirname "$0")/extra/knomi_serial.py" "$EXTRA_PATH"
+echo "Creating Symbolic link to klippy_extras/knomi_serial.py at $EXTRA_PATH"
+ln -s "$(pwd)/$(dirname "$0")/klippy_extras/knomi_serial.py" "$EXTRA_PATH"
 
 # if ! grep -q "klippy/extras/knomi_serial.py" "$HOME/klipper/.git/info/exclude"; then
 #   echo "klippy/extras/knomi_serial.py" >> "$HOME/klipper/.git/info/exclude"
