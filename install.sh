144db77ab86b63381cbc0b45756073e87018e047
Vyolin <94922829+Vylyne@users.noreply.github.com>
2026-06-29 23:39:49 -0400

    Update install script for knomi_serial.py

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-EXTRA_PATH="$HOME/klipper/klippy/extras/zerod.py"
+EXTRA_PATH="$HOME/klipper/klippy/extras/knomi_serial.py"
 if [ -e "$EXTRA_PATH" ]; then
   rm "$EXTRA_PATH"
 fi
-ln -s "$(pwd)/$(dirname "$0")/extra/zerod.py" "$EXTRA_PATH"
+ln -s "$(pwd)/$(dirname "$0")/extra/knomi_serial.py" "$EXTRA_PATH"
 
-if ! grep -q "klippy/extras/zerod.py" "$HOME/klipper/.git/info/exclude"; then
-  echo "klippy/extras/zerod.py" >> "$HOME/klipper/.git/info/exclude"
-fi
+# if ! grep -q "klippy/extras/knomi_serial.py" "$HOME/klipper/.git/info/exclude"; then
+#   echo "klippy/extras/knomi_serial.py" >> "$HOME/klipper/.git/info/exclude"
+# fi
